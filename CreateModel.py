71cb7c421b6a8ccdd0beafd34b71fa1e864ad6c6
Guy Smorodinsky <guy120494@gmail.com>
2025-03-31 12:45:53 +0300

    Fix bug, only first layer can have bias

--- CreateModel.py
+++ CreateModel.py
@@ -51,9 +51,9 @@ class NeuralNetwork(nn.Module):
     def __init__(self, input_dim, hidden_dim_list, output_dim, activation, use_bias=False):
         super().__init__()
         self.activation = activation
-        self.layers = nn.ModuleList([nn.Linear(input_dim, hidden_dim_list[0])])
+        self.layers = nn.ModuleList([nn.Linear(input_dim, hidden_dim_list[0], bias=use_bias)])
         for i in range(1, len(hidden_dim_list)):
-            self.layers.append(nn.Linear(hidden_dim_list[i-1], hidden_dim_list[i], bias=use_bias))
+            self.layers.append(nn.Linear(hidden_dim_list[i-1], hidden_dim_list[i], bias=False))
         self.layers.append(nn.Linear(hidden_dim_list[-1], output_dim, bias=False))  # output layer
 
     def forward(self, data):
