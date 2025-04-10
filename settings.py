0ddb090128d168e02ee92f60ade37f72e22b1e37
Guy Smorodinsky <guy120494@gmail.com>
2025-04-10 09:28:40 +0300

    Correct settings

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
-datasets_dir = r'C:\Users\admin\PycharmProjects\PrivacyRobustness\datasets'
-results_base_dir = r'C:\Users\admin\PycharmProjects\PrivacyRobustness\results'
-models_dir = r'C:\Users\admin\PycharmProjects\PrivacyRobustness\models'
+# datasets_dir = r'C:\Users\admin\PycharmProjects\PrivacyRobustness\datasets'
+# results_base_dir = r'C:\Users\admin\PycharmProjects\PrivacyRobustness\results'
+# models_dir = r'C:\Users\admin\PycharmProjects\PrivacyRobustness\models'
 
-# datasets_dir = r'/home/smorodin/robustnessPrivacy/PrivacyRobustness/datasets'
-# results_base_dir = r'/home/smorodin/robustnessPrivacy/PrivacyRobustness/results'
-# models_dir = r'/home/smorodin/robustnessPrivacy/PrivacyRobustness/models'
+datasets_dir = r'/home/smorodin/robustnessPrivacy/PrivacyRobustness/datasets'
+results_base_dir = r'/home/smorodin/robustnessPrivacy/PrivacyRobustness/results'
+models_dir = r'/home/smorodin/robustnessPrivacy/PrivacyRobustness/models'
