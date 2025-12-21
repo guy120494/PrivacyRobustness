73e47c8034cafd6bd606c3beee8ddf79bbd1c0bb
Guy Smorodinsky <guy120494@gmail.com>
2025-12-21 13:10:49 +0200

    Added option to compare quality of reconstructions between two models

--- margin_evaluation.py
+++ margin_evaluation.py
@@ -13,7 +13,39 @@ from GetParams import str2list
 from Main import setup_args
 from common_utils.common import load_weights
 from evaluations import transform_vmin_vmax_batch
+from settings import results_base_dir
 from utils import get_margin, get_distances_from_margin
+
+import torch
+
+
+def same_images_ignore_order(X: torch.Tensor, Y: torch.Tensor) -> bool:
+    if X.shape != Y.shape:
+        return False
+
+    B = X.shape[0]
+
+    X_flat = X.view(B, -1)
+    Y_flat = Y.view(B, -1)
+
+    X_sorted, _ = torch.sort(X_flat, dim=0)
+    Y_sorted, _ = torch.sort(Y_flat, dim=0)
+    return torch.equal(X_sorted, Y_sorted)
+
+
+def deduplicate_images(x: torch.Tensor):
+    """
+    x: Tensor of shape (B, C, H, W)
+    Returns:
+        unique_images: Tensor of shape (B_unique, C, H, W)
+        inverse_indices: Tensor mapping original indices -> unique indices
+    """
+    B = x.shape[0]
+    x_flat = x.view(B, -1)  # (B, C*H*W)
+    unique_flat, inverse = torch.unique(x_flat, dim=0, return_inverse=True)
+
+    unique_images = unique_flat.view(-1, *x.shape[1:])
+    return unique_images, inverse
 
 
 def pairwise_ssim(imgs1, imgs2, window_size=3):
@@ -56,23 +88,50 @@ def get_evaluation_score_dssim(xxx, yyy):
     return dssim
 
 
-def get_total_successful_reconstructions(path_to_reconstructions_folder: Path, path_to_training_images_file: Path,
-                                         threshold: float = 0.3, device='cuda:0') -> (int, int):
-    training_images = torch.load(str(path_to_training_images_file))['x'].to(device)
-    mean = training_images.mean(dim=[0, -2, -1]).detach()
-    mean = mean.view(1, 3, 1, 1)
+def get_reconstructions_for_training_images(path_to_reconstructions_folder: Path, training_images, mean,
+                                            device='cuda:0'):
+    reconstructions = torch.zeros_like(training_images)
+    best_dssim = torch.full((training_images.shape[0],), float('inf'), dtype=torch.float64).to(device)
+    for file_path in path_to_reconstructions_folder.rglob('**/*x*.pt*'):
+        reconstructed_images = get_reconstructed_images(file_path, device)
+        for i, batch_data in enumerate(reconstructed_images):
+            current_batch = batch_data[0] + mean
+            dssim_matrix = get_evaluation_score_dssim(current_batch, training_images)
+            col_min_val, col_min_idx = dssim_matrix.min(dim=0)
+            mask = col_min_val < best_dssim
+            best_dssim[mask] = col_min_val[mask]
+            col_min_val = col_min_val.masked_fill(~mask, float('inf'))
+            mask = torch.isfinite(col_min_val)
+            reconstructions[mask] = current_batch[col_min_idx[mask]]
+
+    return reconstructions
+
+
+def get_successful_reconstructions(path_to_reconstructions_folder: Path, training_images,
+                                   mean, threshold: float = 0.3, device='cuda:0'):
+    successful_reconstructions = []
+    reconstructed_training_images = []
+    for file_path in path_to_reconstructions_folder.rglob('**/*x*.pt*'):
+        reconstructed_images = get_reconstructed_images(file_path, device)
+        for i, batch_data in enumerate(reconstructed_images):
+            current_batch = batch_data[0] + mean
+            dssim_matrix = get_evaluation_score_dssim(current_batch, training_images)
+            row_min_vals, row_min_idx = dssim_matrix.min(dim=1)
+            mask = row_min_vals < threshold
+            successful_reconstructions.extend(torch.unbind(current_batch[mask]))
+            reconstructed_training_images.extend(torch.unbind(training_images[row_min_idx[mask]]))
+
+    return torch.stack(successful_reconstructions, dim=0), torch.stack(reconstructed_training_images, dim=0)
+
+
+def get_total_number_of_successful_reconstructions(path_to_reconstructions_folder: Path, training_images, mean,
+                                                   threshold: float = 0.3,
+                                                   device='cuda:0') -> (int, int):
     successful_reconstructions = []
     number_of_attacks = 0
-
     for file_path in path_to_reconstructions_folder.rglob('**/*x*.pt*'):
         number_of_attacks += 1
-        reconstructed_images = TensorDataset(torch.load(str(file_path)).to(device))
-        reconstructed_images = DataLoader(
-            reconstructed_images,
-            batch_size=10,
-            shuffle=False,  # Set to True for training
-            drop_last=False
-        )
+        reconstructed_images = get_reconstructed_images(file_path, device)
         successful_batch = []
         for i, batch_data in enumerate(reconstructed_images):
             current_batch = batch_data[0]
@@ -85,6 +144,17 @@ def get_total_successful_reconstructions(path_to_reconstructions_folder: Path, p
     total_number_of_successful_reconstructions = torch.stack(successful_reconstructions, dim=0).any(
         dim=0).sum().detach().item()
     return total_number_of_successful_reconstructions, number_of_attacks
+
+
+def get_reconstructed_images(file_path, device):
+    reconstructed_images = TensorDataset(torch.load(str(file_path)).to(device))
+    reconstructed_images = DataLoader(
+        reconstructed_images,
+        batch_size=10,
+        shuffle=False,  # Set to True for training
+        drop_last=False
+    )
+    return reconstructed_images
 
 
 def print_histogram(data, bins=10, width=50, header=None):
@@ -165,6 +235,9 @@ def get_args(*args):
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--train_file', type=str)
     parser.add_argument('--reconstruction_folder', type=str)
+    parser.add_argument('--first_reconstruction_folder', type=str)
+    parser.add_argument('--second_reconstruction_folder', type=str)
+    parser.add_argument('--save_folder', type=str)
     parser.add_argument('--model', type=str, default='')
     parser.add_argument('--threshold', type=float, default=0.3)
     parser.add_argument('--run_mode', type=str, default='reconstruct')
@@ -194,18 +267,36 @@ if __name__ == '__main__':
     args = get_args(sys.argv[1:])
     path_to_reconstructions_folder = Path(args.reconstruction_folder)
     path_to_training_images_file = Path(args.train_file)
+    training_images = torch.load(str(path_to_training_images_file))['x'].to(args.device)
+    mean = training_images.mean(dim=[0, -2, -1]).detach()
+    mean = mean.view(1, 3, 1, 1)
+
     print(f"DSSIM THRESHOLD {args.threshold}")
     print(f"RECONSTRUCTION FOLDER {path_to_reconstructions_folder}")
     print(f"TRAINING IMAGES {path_to_training_images_file}")
-    thresholds = [0.2 + i * 0.05 for i in range(5)]
-    points = []
-    for threshold in thresholds:
-        y = get_total_successful_reconstructions(path_to_reconstructions_folder, path_to_training_images_file,
-                                                 threshold=threshold)[0]
-        points.append((threshold, y))
-    print_graph(points)
-    print([p[0] for p in points])
-    print([p[1] for p in points])
+
+    if args.analyze_multiple_thresholds:
+        thresholds = [0.2 + i * 0.05 for i in range(5)]
+        points = []
+        for threshold in thresholds:
+            y = get_total_number_of_successful_reconstructions(path_to_reconstructions_folder, training_images, mean,
+                                                               threshold=threshold)[0]
+            points.append((threshold, y))
+        print([p[0] for p in points])
+        print([p[1] for p in points])
+
+    if args.first_reconstruction_folder is not None and args.second_reconstruction_folder is not None:
+        path_to_first_reconstruction_folder = Path(args.first_reconstruction_folder)
+        path_to_second_reconstruction_folder = Path(args.second_reconstruction_folder)
+        _, t1 = get_successful_reconstructions(path_to_first_reconstruction_folder, training_images, mean,
+                                               args.threshold)
+        # t1 = deduplicate_images(t1)[0]
+        y1 = get_reconstructions_for_training_images(path_to_first_reconstruction_folder, t1, mean)
+        y2 = get_reconstructions_for_training_images(path_to_second_reconstruction_folder, t1, mean)
+
+        torch.save(
+            {"reconstructed_training": t1, "first_model_reconstructions": y1, "second_model_reconstructions": y2},
+            Path(results_base_dir) / args.save_folder / "compare_models.pth")
 
     # torch.set_default_dtype(torch.float64)
     # model = create_model(args, extraction=True)
